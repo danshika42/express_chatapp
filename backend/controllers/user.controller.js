a9de92f0bf78c065c044cdf7d02a392208ee16fd
Dubey <anshika.d.dubey@accenture.com>
2024-10-10 18:57:28 +0530

    --added backend

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,14 @@
+import User from "../models/user.model.js";
+
+export const getUserForSidebar= async (req,res)=>{
+    try{
+        const loggedInId=req.user._id;
+        const filteredUsers=await User.find({_id:{$ne:loggedInId}}).select("-password");
+
+        res.status(200).json(filteredUsers);
+
+    }catch(error){
+        console.log("Error in user controller : ",error.message);
+        res.status(500).json({error:"Internal server error"});
+    }
+}
