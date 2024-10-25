95a47183c157718b147e5fdcc65d6c293779a6e3
Dubey <anshika.d.dubey@accenture.com>
2024-10-25 15:58:17 +0530

    --changed deployed url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider=({children})=>{
 
     useEffect(()=>{
         if(authUser){
-            const socket=io("http://localhost:8000",{
+            const socket=io("https://express-chatapp-ez1d.onrender.com",{
                 query:{
                     userId:authUser._id,
                 },
